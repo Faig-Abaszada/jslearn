4643fb0003b28eb0bf9bfdc1045d1f7c392179e2
Faig-Abaszada <faik.abaszada@gmail.com>
2021-06-13 13:36:31 +0400

    chapter_10 * eventin keanirda generateTemplate yaratdiq cunki to do html-i yazmaq   isteyirik ama bunun hemde reusable olmasini isteyirik

--- chapter_10/app.js
+++ chapter_10/app.js
@@ -1,9 +1,27 @@
 const addForm = document.querySelector('.add');
+// li yaratdiq ama bunu ul-nin icinde qoymaq ucun grabbed etmeliyik
+const list = document.querySelector('.todos');
+
+/*
+eventin keanirda generateTemplate yaratdiq cunki to do html-i yazmaq
+isteyirik ama bunun hemde reusable olmasini isteyirik
+ */
+const generateTemplate = todo => {
+    // burada create metodu ilede yarada bilerdik ama templ string ile etdik
+    const html = `
+         <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+    list.innerHTML += html;
+};
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const todo = addForm.add.value.trim();
-    console.log(todo);
+    generateTemplate(todo);
 })
 
 // trim() butun white space-leri silir inputda qoyulan
+
