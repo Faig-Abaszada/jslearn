e2f6d855504b4345a43f4b05fb5de8839c70b386
Faig-Abaszada <faik.abaszada@gmail.com>
2021-06-13 13:23:28 +0400

    chapter_10 * trim() butun white space-leri silir inputda qoyulan

--- chapter_10/app.js
+++ chapter_10/app.js
@@ -2,6 +2,8 @@ const addForm = document.querySelector('.add');
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const todo = addForm.add.value;
+    const todo = addForm.add.value.trim();
     console.log(todo);
 })
+
+// trim() butun white space-leri silir inputda qoyulan
