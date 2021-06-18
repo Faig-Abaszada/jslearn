afba2d2992faf2ad357227a59318a890f684982a
Faig-Abaszada <faik.abaszada@gmail.com>
2021-06-18 19:23:50 +0400

    chapter_12 || weather app continue...

--- chapter_13/weatherApp/scripts/app.js
+++ chapter_13/weatherApp/scripts/app.js
@@ -2,7 +2,16 @@
 
  const cityForm = document.querySelector('form');
 
+
+
 cityForm.addEventListener('submit', (e) => {
+    // prevent default action
     e.preventDefault();
+
+    // get city value to show weather
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // updates ui with new city
 
 })
