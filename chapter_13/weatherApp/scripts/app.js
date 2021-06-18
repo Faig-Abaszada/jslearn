9d2504f174fd14347a954c587240eb4a7d406f5f
Faig-Abaszada <faik.abaszada@gmail.com>
2021-06-18 19:57:51 +0400

    chapter_12 || weather app continue...

--- chapter_13/weatherApp/scripts/app.js
+++ chapter_13/weatherApp/scripts/app.js
@@ -1,6 +1,24 @@
 // dom manipullation
 
  const cityForm = document.querySelector('form');
+// indexde deyiseceyimiz ana divleri secdik
+const card = document.querySelector('.card');
+const details = document.querySelector('.details ');
+
+const updateUI = (data) => {
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+             <h5 class="my-3">${cityDets.EnglishName}</h5>
+            <div class="my-3">${weather.WeatherText }</div>
+            <div class="display-4 my-4">
+                <span>${weather.Temperature.Metric.Value}</span>
+                <span>&deg;${weather.Temperature.Metric.Unit }</span>
+            </div>
+    `
+}
 
 const  updateCity = async (city) => {
     // console.log(city);
@@ -28,6 +46,6 @@ cityForm.addEventListener('submit', (e) => {
 
     // updates ui with new city
     updateCity(city)
-        .then(data =>  console.log(data))
+        .then(data =>  updateUI(data))
         .catch(err => console.log(err));
 })
