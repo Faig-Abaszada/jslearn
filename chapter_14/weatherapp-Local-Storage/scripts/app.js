d9fd64554f035cfc8d0989e382e41288f70cd37d
Faig-Abaszada <faik.abaszada@gmail.com>
2021-06-22 08:27:27 +0400

    chapter_14 || LocalStorage continue || weatherapp to localstorage

---
+++ chapter_14/weatherapp-Local-Storage/scripts/app.js
@@ -0,0 +1,86 @@
+// dom manipullation
+
+ const cityForm = document.querySelector('form');
+// indexde deyiseceyimiz ana divleri secdik
+const card = document.querySelector('.card');
+const details = document.querySelector('.details ');
+// imgs icons grab elements
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    // bezi melumatlari
+    console.log(data);
+
+    // destructure data
+    const { cityDets, weather } = data;
+
+    // update details template
+    details.innerHTML = `
+             <h5 class="my-3">${cityDets.EnglishName}</h5>
+            <div class="my-3">${weather.WeatherText }</div>
+            <div class="display-4 my-4">
+                <span>${weather.Temperature.Metric.Value}</span>
+                <span>&deg;${weather.Temperature.Metric.Unit }</span>
+            </div>
+    `
+    // update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    // console.log(icon);
+
+    let timeSrc = null;
+    if (weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
+
+    // remove d-none class
+    if (card.classList.contains('d-none')) {
+         card.classList.remove('d-none');
+    }
+}
+
+const  updateCity = async (city) => {
+    // console.log(city);
+
+    // city code elde etdik
+    const cityDets = await getCity(city);
+    // city code verib weather elde etdik
+    const weather = await  getWeather(cityDets.Key);
+
+    // console.log(cityDets, weather);
+    return {
+        cityDets,
+        weather
+    };
+
+}
+
+cityForm.addEventListener('submit', (e) => {
+    // prevent default action
+    e.preventDefault();
+
+    // get city value to show weather
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // updates ui with new city
+    updateCity(city)
+        .then(data =>  updateUI(data))
+        .catch(err => console.log(err));
+
+    // set localStorage
+     localStorage.setItem('city', city);
+})
+
+if (localStorage.getItem('city')){
+    updateCity(localStorage.getItem('city'))
+        .then( data => updateUI(data))
+        .catch(err => console.log(err));
+}
---
+++ chapter_14/weatherapp-Local-Storage/scripts/forecast.js
@@ -0,0 +1,63 @@
+
+const apiKey = 'C5A1SfhvUXNbyF5VtwWiJWiPDxdYx73Y';
+
+//get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    // buradaki id 'Current Conditions'-da teleb olunandir
+    // api key ise teleb olunan query-dir
+    const query = `${id}?apikey=${apiKey}`;
+
+    const response = await fetch(base + query);
+    const data = await  response.json();
+
+    // data[0] deyirik cunki helede geleden data array icindedir bize ise object lazimdir.
+    return data[0];
+}
+
+
+
+// get city information
+const getCity = async (city) => {
+
+    /*
+     - ?(question mark) - menasi adding parameter to the url
+     - & - next query paramater adding
+     - accuweathe saytinda required olan iki query var apikey ve q
+    */
+
+    // bu endpoint city code elde etmek ucun olan endpointdir sonra bu kod ile uygun citynin havasini oyrenecyik
+    // endpoint + query( ve elave bizim apikey ve input ile yazagaimiz city adi )
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${apiKey}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await  response.json();
+
+    // console.log(data);
+    // output olaraq birden cox objectin consola yazildigini
+    // goruruk bize lazim olan 1cidir cunki digerleri ozunden atdigi sheherlerdir
+    // console.log(data[0]);
+
+    return data[0];
+
+}
+
+//
+// // ve burada bi ze lazim olan shey keydir, consolda objecti acsaq key deyerini  goererik
+// getCity('baku')
+//     .then((data) => {
+//         // console.log(data);
+//         /* ve data bize sheherin codunu verir data.Key olaraq
+//            bizde bu key ile hemin sheherin melumatlarini aldiq.
+//            - return deyerken bu artiq promise return edir ve artiq biz .then() etmleiyik
+//
+//         */
+//         return getWeather(data.Key);})
+//     .then((data) => {
+//         //buraddaki data artiq getWeather icindeki data[0]- dir
+//         console.log(data);})
+//     .catch((err) => {
+//         console.log(err)
+//     });
