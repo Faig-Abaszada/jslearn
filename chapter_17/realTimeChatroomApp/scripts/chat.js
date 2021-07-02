e1a0c2c7420c8ecf4cf000e0f6635ce6760cebb9
Faig-Abaszada <faik.abaszada@gmail.com>
2021-07-02 23:49:11 +0400

    chapter_17 ||

--- chapter_17/realTimeChatroomApp/scripts/chat.js
+++ chapter_17/realTimeChatroomApp/scripts/chat.js
@@ -1,9 +1,4 @@
-// adding new chat documents
-// setting up a real-time listener to get new chats
-// updating the username
-// updating the room
 
-// this responsible for managing all of the chatroom data
 class Chatroom  {
     constructor(room, username) {
         this.room = room;
@@ -11,23 +6,19 @@ class Chatroom  {
         this.chats = db.collection('chats');
         this.unsub;
     }
-    async addChat(message) { // metod 1 , async-dir
-        // format a chat object
+    async addChat(message) {
         const now = new Date();
-        // making a chat object data comes from Chatroom class
         const chat = {
             message: message,
             username: this.username,
             room: this.room,
             created_at: firebase.firestore.Timestamp.fromDate(now),
         };
-        // save the chat documents
         const response = await this.chats.add(chat);
-        // cox ehtimal return responsa ehtiyac olmayacaq, bu chainin ucundur
         return response;
     }
     getChats(callback) {
-        this.unsub = this.chats // ve unsub call oldugunda changes unsubscribe olunur. ve
+        this.unsub = this.chats
             .where('room', '==', this.room)
             .orderBy('created_at')
             .onSnapshot(snapshot => {
@@ -42,10 +33,8 @@ class Chatroom  {
     updateUsername(username) {
         this.username = username;
     }
-    /*
-     - burada room-u update etsekde getChats-da biz helede initial room-u goruruk
-    */
-    updateRoom(room) { // bu updateRoom yalniz propertyni update edib listeningi ise unsub etmekdir.
+
+    updateRoom(room) {
         this.room = room;
         console.log('room updated');
         if (this.unsub) {
@@ -53,31 +42,16 @@ class Chatroom  {
         }
     }
 }
-
-/*
- - new chatroom ile clasimizin instancesini yaratdiq ve room , username-ni verdik
- - verdikden sonra chatroom.addChat() deyerek metodu cagirib mesajimizi yazdiq
- */
-const chatroom = new Chatroom('music', 'faik'); // buradaki room-u goruruk helede
-
-// chatroom.addChat('hello everyone').then(() => {
-//     console.log('chat added');
-// }).catch(err => {
-//     console.log(err);
-// });
+const chatroom = new Chatroom('music', 'faik');
 
 chatroom.getChats((data) => {
     console.log(data);
 });
 
-
-
-
-
 setTimeout(() => {
     chatroom.updateRoom('gaming');
     chatroom.updateUsername('AliDev');
-    chatroom.getChats((data) => { //davami olaraq getChats-i yeniden call etmeliyik.
+    chatroom.getChats((data) => {
         console.log(data);
     });
     chatroom.addChat('hello');
