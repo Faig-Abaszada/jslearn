a2581d8ac5ea8a386e1e13c8c7464e40bdb3ef10
Faig-Abaszada <faik.abaszada@gmail.com>
2021-07-04 10:41:48 +0400

    chapter_17 || update username

--- chapter_17/realTimeChatroomApp/scripts/app.js
+++ chapter_17/realTimeChatroomApp/scripts/app.js
@@ -6,6 +6,7 @@
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
 // add new chat
 newChatForm.addEventListener('submit', e => {
@@ -21,10 +22,19 @@ newChatForm.addEventListener('submit', e => {
 // update username
 newNameForm.addEventListener('submit', e => {
     e.preventDefault();
-
+    // update name  also chatroom
     const newName = newNameForm.name.value.trim();
+    chatroom.updateUsername(newName);
+    // reset the form
+    newNameForm.reset();
+    // show updated username message
+    updateMssg.innerHTML = `
+        Your name was updated to ${newName}
+    `;
 
-    chatroom.updateUsername(name);
+    setTimeout(() => { // settime out ile 3 san sonra yazini sildik ekrandan.
+        updateMssg.innerHTML = '';
+    }, 3000);
 })
 
 
