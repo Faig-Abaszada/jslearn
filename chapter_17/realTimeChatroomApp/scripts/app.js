d5dd330289bfc59a1ac0f804c77f0c4e848a1838
Faig-Abaszada <faik.abaszada@gmail.com>
2021-07-04 09:56:06 +0400

    chapter_17 || Send data to db

--- chapter_17/realTimeChatroomApp/scripts/app.js
+++ chapter_17/realTimeChatroomApp/scripts/app.js
@@ -11,7 +11,9 @@ newChatForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = newChatForm.message.value.trim();
 
-    Chatroom.addChat(message);
+    Chatroom.addChat(message) // ve unutmayaqki addChat()- bir async funcdir ve asynler her zaman promise return edir buna gore .then ist etmeliyik
+        .then(() => { newChatForm.reset() })
+        .catch(err => console.log(err))
 
 });
 
